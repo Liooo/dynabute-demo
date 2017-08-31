c9c25b5e8951edb5021fabe1fe06de64beeab16a
Liooo <ryoyamada3@gmail.com>
2017-08-31 23:36:37 +0900

    table’s working

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def dynabute_value_text(value, field)
+    return '' if value.blank?
+    if field.has_many
+      value.map{|v| value_text(v, field) }.join(',')
+    else
+      value_text(value, field)
+    end
+  end
+
+  private
+  def value_text(value, field)
+    field.value_type == 'select' ? value.option.label : value.value
+  end
 end
